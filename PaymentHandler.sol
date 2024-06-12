4312bb0207c601dece4883a8b9f044c43187db43
Manjunath Gudditi <manjunath@elasticdevs.io>
2024-06-12 14:19:11 +0530

    updated PaymentHandler contract

--- PaymentHandler.sol
+++ PaymentHandler.sol
@@ -3,40 +3,74 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract PaymentHandler is Ownable, ReentrancyGuard {
-    address public fundsHandler;
-    mapping(address => uint256) public payerfund;
-    address[] payers;
-    // Events
-    event PaymentReceived(address indexed sender, uint256 amount, address indexed receiver);
+    address public usdcTokenAddress;
+    address public usdtTokenAddress;
+    address[] USDCpayers;
+    address[] USDTpayers;
     
-    constructor(address initialOwner, address initialFundsHandler) Ownable(initialOwner) {
+    mapping(address => mapping (address => uint256)) public tokenPayer;
+
+    // Events
+    event TokenReceived(address indexed _sender, uint256 _amount, address indexed _tokenAddress);
+    event WithdrawToken(address indexed _owner, address indexed _tokenAddress, address indexed _receiver, uint256 _value);
+
+    constructor(address initialOwner, address _usdcTokenAddress, address _usdtTokenAddress) Ownable(initialOwner) {
         require(initialOwner != address(0), "Invalid initial owner address");
-        require(initialFundsHandler != address(0), "Invalid initial funds handler address");
-        fundsHandler = initialFundsHandler;
+        require(_usdcTokenAddress != address(0), "Invalid usdc token address");
+        require(_usdtTokenAddress != address(0), "Invalid usdt token address");
+        usdcTokenAddress = _usdcTokenAddress;
+        usdtTokenAddress = _usdtTokenAddress;
+    }
+    
+    function onTokenTransferUSDC(uint256 _amount) public {
+        require(ERC20(usdcTokenAddress).balanceOf(msg.sender) >= _amount, "Insufficient token");
+        require(ERC20(usdcTokenAddress).allowance(msg.sender,address(this)) >= _amount,"Not approved");
+        ERC20(usdcTokenAddress).transferFrom(msg.sender, address(this), _amount);
+        USDCpayers.push(msg.sender);
+        tokenPayer[msg.sender][usdcTokenAddress] += _amount;
+        emit TokenReceived(msg.sender,_amount,usdcTokenAddress);
     }
 
-    function pay() public payable nonReentrant {
-        require(msg.value > 0, "Payment zero");
-        require(fundsHandler != address(0), "Funds handler not set");
-        (bool success, ) = payable(fundsHandler).call{value: msg.value}("");
-        require(success, "Payment failed");
-        payers.push(msg.sender);
-        payerfund[msg.sender] += msg.value;
-        emit PaymentReceived(msg.sender, msg.value,fundsHandler);
+    function onTokenTransferUSDT(uint256 _amount) public {
+        require(ERC20(usdtTokenAddress).balanceOf(msg.sender) >= _amount, "Insufficient token balance");
+        require(ERC20(usdtTokenAddress).allowance(msg.sender,address(this)) >= _amount,"Not approved");
+        ERC20(usdtTokenAddress).transferFrom(msg.sender, address(this), _amount);
+        USDTpayers.push(msg.sender);
+        tokenPayer[msg.sender][usdtTokenAddress] += _amount;
+        emit TokenReceived(msg.sender,_amount,usdtTokenAddress);
+    }
+    
+    function withdrawUSDCToken(address _to, uint256 _amount) external nonReentrant onlyOwner {
+        uint token = getContractUSDCTokenBalance();
+        require( token >= _amount,"Insufficient token balance");
+        ERC20(usdcTokenAddress).transfer(_to, _amount);
+        emit WithdrawToken(msg.sender, usdcTokenAddress, _to, _amount);
     }
 
-    function setFundsHandler(address _newFundsHandler) external onlyOwner {
-        require(_newFundsHandler != address(0), "Invalid address");
-        fundsHandler = _newFundsHandler;
+    function withdrawUSDTToken(address _to, uint256 _amount) external nonReentrant onlyOwner {
+        uint token = getContractUSDTTokenBalance();
+        require( token >= _amount,"Insufficient token balance");
+        ERC20(usdtTokenAddress).transfer(_to, _amount);
+        emit WithdrawToken(msg.sender, usdtTokenAddress, _to, _amount);
     }
 
-    function getPayers() public view returns(address[] memory){
-        return payers;
+    //getter functions
+    function getContractUSDCTokenBalance() public view returns (uint) {
+         return ERC20(usdcTokenAddress).balanceOf(address(this));
+    }
+
+    function getContractUSDTTokenBalance() public view returns (uint) {
+         return ERC20(usdtTokenAddress).balanceOf(address(this));
+    }
+
+    function getUSDCpayers() public view returns (address[] memory){
+        return USDCpayers;
+    }
+
+    function getUSDTpayers() public view returns (address[] memory){
+        return USDTpayers;
     }
 }
-
-
-
-
