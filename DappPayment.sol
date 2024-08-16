cad56f6afe474a7d1428ee66b02cb0941a4ccc86
Aryan Mishra <aryanmishracoc@gmail.com>
2024-08-16 12:48:05 +0530

    bug fix

--- DappPayment.sol
+++ DappPayment.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-contract PaymentHandler is Ownable, ReentrancyGuard {
+contract DappPayment is Ownable, ReentrancyGuard {
     using SafeERC20 for IERC20;
     address public usdcTokenAddress;
     address public usdtTokenAddress;
