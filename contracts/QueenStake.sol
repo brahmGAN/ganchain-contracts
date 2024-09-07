33ce225a7374b0137df16a6a5854a690ad9b528e
Deepu <deepuharry02@gmail.com>
2024-09-07 10:09:05 +0530

    on going

--- contracts/QueenStake.sol
+++ contracts/QueenStake.sol
@@ -12,6 +12,9 @@ contract QueenStaking is OwnableUpgradeable, UUPSUpgradeable, ReentrancyGuardUpg
     /// @dev Timestamp of the last rewards calculated at 
     uint40 public _lastRewardCalculated; 
 
+    /// @dev Timestamp of staking health last set 
+    uint40 public _stakingHealthSetAt; 
+
     /// @dev The rewards set aside for the entire queen nodes pool per day 
     /// @dev Can hold up to 100 million rewards in GPoints per day, denominated in wei
     uint88 public _rewardsPerDay; 
@@ -23,7 +26,7 @@ contract QueenStaking is OwnableUpgradeable, UUPSUpgradeable, ReentrancyGuardUpg
     //GPU public GPUInstance;
 
     // Not yet finalized 
-    mapping(address => uint) _stakeHealth; 
+    mapping(address => uint) _stakingHealth; 
 
     /// @dev Maps the amount staked by a particular queen node 
     mapping(address => uint88) _stakedAmount;
@@ -35,6 +38,9 @@ contract QueenStaking is OwnableUpgradeable, UUPSUpgradeable, ReentrancyGuardUpg
     /// @dev Queen's rewards which is calculated every day
     mapping(address => uint) _queenRewards;
 
+    /// @dev List of queens that stakes
+    address[] public _queens; 
+
     /// @dev Authorizes the upgrade to a new implementation. Only callable by the owner.
     function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
 
@@ -45,9 +51,9 @@ contract QueenStaking is OwnableUpgradeable, UUPSUpgradeable, ReentrancyGuardUpg
         __UUPSUpgradeable_init();
         __ReentrancyGuard_init();
         _nftContract = IERC721(nftContract);
-        //_lastRewardAt = uint40(block.timestamp);
         _rewardsPerDay = uint88(rewardsPerDay);
         _lastRewardCalculated = uint40(block.timestamp); 
+        _stakingHealthSetAt = uint40(block.timestamp);
     }
 
     /// @dev Allows the users to stake and become a queen node.
@@ -62,6 +68,7 @@ contract QueenStaking is OwnableUpgradeable, UUPSUpgradeable, ReentrancyGuardUpg
         }
         _totalStakes += uint96(msg.value); 
         _stakedAmount[msg.sender] += uint88(msg.value); 
+        _queens.push(msg.sender);
         //@note emit
     }  
 
@@ -74,8 +81,31 @@ contract QueenStaking is OwnableUpgradeable, UUPSUpgradeable, ReentrancyGuardUpg
         //@note emit
     }
 
+    function setStakingHealth(uint[] memory stakingHealth) public onlyOwner {
+        if(block.timestamp < _stakingHealthSetAt + 24 hours) revert InComplete24Hours();
+        address[] memory queens = _queens; 
+        uint queensLength = queens.length; 
+        for(uint i = 0; i < queensLength; i++) {
+            _stakingHealth[queens[i]] = stakingHealth[i];
+        }
+        _stakingHealthSetAt = uint40(block.timestamp);
+    }
+
     function accumulateDailyQueenRewards() public onlyOwner {
         if(block.timestamp < _lastRewardCalculated + 24 hours) revert InComplete24Hours();
+        address[] memory queens = _queens; 
+        uint totalQueens = queens.length; 
+        uint256[] memory stakeScores = new uint256[](totalQueens); 
+        uint256 totalStakeScore;
+        for(uint i = 0; i < totalQueens; i++) {
+            // Stake score = SU * SM * SH 
+            stakeScores[i] = _stakedAmount[msg.sender] * _stakingHealth[queens[i]]; 
+        }
+    }
+
+    function stakingMultiplier(address queen) public {
+        uint8 factor = 100; 
+        uint stakedAmount = _stakedAmount[queen]; 
         
     }
 
