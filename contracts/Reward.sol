ccd32967f68b3dc23210d1b000397236cb0b40dc
Deepu <deepuharry02@gmail.com>
2025-02-24 21:13:37 +0530

    optimized provider rewards storage wise

--- contracts/Reward.sol
+++ contracts/Reward.sol
@@ -10,13 +10,13 @@ import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol
 contract Reward is OwnableUpgradeable, UUPSUpgradeable, ReentrancyGuardUpgradeable {
 
     GPU public GPUInstance;
-    uint256 lastRewardCalculated;
-    uint256 rewardGPsPerDay;
-    uint256 LOCK_PERIOD;
+    uint40 lastRewardCalculated;
+    uint96 rewardGPsPerDay;
+    uint24 constant LOCK_PERIOD = 30 days;
 
     // Mappings
-    mapping (address => uint256) public providerRewards;
-    mapping (address => uint256) public lastWithdrawalTime;
+    mapping (address => uint96) public providerRewards;
+    mapping (address => uint40) public lastWithdrawalTime;
 
     // Events
     event DailyProviderRewardsAccumulated(address owner, uint256 timestamp);
@@ -25,14 +25,14 @@ contract Reward is OwnableUpgradeable, UUPSUpgradeable, ReentrancyGuardUpgradeab
     // Functions
     function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
 
-    function initialize(address _GPUAddress, uint256 RewardGPsPerDay) public initializer {
+    function initialize(address _GPUAddress, uint96 RewardGPsPerDay) public initializer {
         __Ownable_init(msg.sender);
         __UUPSUpgradeable_init();
         __ReentrancyGuard_init();
         GPUInstance = GPU(_GPUAddress);
-        lastRewardCalculated = block.timestamp;
+        lastRewardCalculated = uint40(block.timestamp);
         rewardGPsPerDay = RewardGPsPerDay;
-        LOCK_PERIOD = 30 days;
+        //LOCK_PERIOD = 30 days;
     }
 
     function accumulateDailyProviderRewards() public onlyOwner {
@@ -42,27 +42,28 @@ contract Reward is OwnableUpgradeable, UUPSUpgradeable, ReentrancyGuardUpgradeab
         address[] memory providers = GPUInstance.getProviders();
         uint256[] memory computeScores = new uint256[](providers.length);
         uint256 totalComputeScore = 0;
-        for (uint256 i = 0; i < providers.length; i++) {
+        uint24 providersLength = uint24(providers.length); 
+        for (uint24 i = 0; i < providersLength; i++) {
             (uint256 computeUnits, uint256 healthScore) = GPUInstance.getProviderComputeDetails(providers[i]);
-            uint256 computeMultiplier = getComputeMultiplier(computeUnits);
+            uint8 computeMultiplier = getComputeMultiplier(computeUnits);
             // Compute Score (CS) = CU * CM * CH
             computeScores[i] = (computeUnits * computeMultiplier * healthScore);
             //100 for computeMultiplier, 10 for computeUnits, 10 for healthScore
             totalComputeScore += computeScores[i];
         }
         if (totalComputeScore > 0) { // Avoid division by zero
-            for (uint256 i = 0; i < providers.length; i++) {
+            for (uint24 i = 0; i < providersLength; i++) {
                 // P’s reward per day = (Pcs / ∑Pcs )  * 1152  GP
                 uint256 todaysReward = (computeScores[i] * rewardGPsPerDay * 10**18) / (totalComputeScore); // In wei
                 address providerNFTAddress = GPUInstance.getNFTAddress(providers[i]);
-                providerRewards[providerNFTAddress] += todaysReward;
+                providerRewards[providerNFTAddress] += uint96(todaysReward);
             }
         }
-        lastRewardCalculated = block.timestamp;
+        lastRewardCalculated = uint40(block.timestamp);
         emit DailyProviderRewardsAccumulated(msg.sender, block.timestamp);
     }
 
-    function getComputeMultiplier(uint256 computeUnits) public pure returns(uint256) {
+    function getComputeMultiplier(uint256 computeUnits) public pure returns(uint8) {
         if (computeUnits < 81) {
             return 100;
         } else if (computeUnits < 641) {
@@ -84,7 +85,7 @@ contract Reward is OwnableUpgradeable, UUPSUpgradeable, ReentrancyGuardUpgradeab
         providerRewards[msg.sender] = 0;
         (bool success,) = payable(msg.sender).call{value: _providerRewards}("");
         require(success, "TransferFailed");
-        lastWithdrawalTime[msg.sender] = block.timestamp;
+        lastWithdrawalTime[msg.sender] = uint40(block.timestamp);
         emit RewardWithdrawn(msg.sender, providerRewards[msg.sender]);
     }
 }
